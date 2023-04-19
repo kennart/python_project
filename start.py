e6716abf021cd5442cb7ddebef65dfd6a072c0be
kennart <foundationtech2@gmail.com>
2023-04-19 04:20:14 -0500

    added another if statement and comments

--- start.py
+++ start.py
@@ -1,8 +1,12 @@
+# This program takes user input and evaluate them to get the answer
+
 first_number = int(input('Please enter your first number: '))
 operation = input('Please enter operation: + or - or * or /: ')
 second_number = int(input('Please enter your second number: '))
 
 if operation == '+':
     print(first_number + second_number)
+if operation == '-':
+    print(first_number - second_number)
 else:
     print('invalid number or operation')
